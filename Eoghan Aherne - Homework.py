16e2b1befcbbf9350c8e49cfa4ea7878504069d7
Eoghan41 <eoghanaherne@hotmail.com>
2015-10-10 22:54:33 +0100

    #2 update
    Added the SQL queries, the connrction to the database and created arrays

--- Eoghan Aherne - Homework.py
+++ Eoghan Aherne - Homework.py
@@ -8,3 +8,12 @@ Created on Sat Oct 10 22:45:06 2015
 import numpy as np #import numpy
 import pandas as pd # import pandas
 import sqlite3 # import sqlite
+
+conn = sqlite3.connect('renewable.db')  # connect to the Database
+
+location=("SELECT * FROM location;") # select all the columns from the 'location' table
+ports=("SELECT * FROM ports;") # select all the columns from the 'ports'table
+
+costs=[] #create an empty array 'costs'
+locations=[] #create an empty array 'locations'
+portslocation =[] #create an empty array 'portlocations'
