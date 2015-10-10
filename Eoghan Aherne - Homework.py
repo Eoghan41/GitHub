3ed4a526e821f9f55fe5ff4e8cb4c5db8f874543
Eoghan41 <eoghanaherne@hotmail.com>
2015-10-10 22:59:56 +0100

    #Update 3
    Created dataframes and Matrices

--- Eoghan Aherne - Homework.py
+++ Eoghan Aherne - Homework.py
@@ -17,3 +17,11 @@ ports=("SELECT * FROM ports;") # select all the columns from the 'ports'table
 costs=[] #create an empty array 'costs'
 locations=[] #create an empty array 'locations'
 portslocation =[] #create an empty array 'portlocations'
+
+df=pd.io.sql.read_sql(location,conn) # crate a dataframe named 'df' with the Pandas library 'read_sql' with location and conn as the parameters
+df2=pd.io.sql.read_sql(ports,conn) # crate a dataframe named 'df2' with the Pandas library 'read_sql' with ports and conn as the parameters
+
+col=df.as_matrix() # create matrix out of dataframes
+col2=df2.as_matrix()
+#print col # look at what the matrices look like
+#print col2
