a8e93a9e75fe41fccf33b6d5d6787a0afe4575ff
Eoghan41 <eoghanaherne@hotmail.com>
2015-10-10 23:03:54 +0100

    #5 Update - Final
    Created the formula to solve the minimzation of costs problem and
    created a print statement with the answer

--- Eoghan Aherne - Homework.py
+++ Eoghan Aherne - Homework.py
@@ -27,3 +27,15 @@ LocationProduction=np.array( df.iloc[0:9,2]) # create an array 'LocationProducti
 
 PortsLatitude=np.array (df2.iloc[0:9,0]) #  create an array 'PortsLatitude' with the Numpy libray, consisting of the first 0 to 9 rows of the 0th row of the dataframe 'df2'
 PortsLongitude=np.array( df2.iloc[0:9,1])# create an array 'PortsLongitude' with the Numpy libray, consisting of the first 0 to 9 rows of the 1st row of the dataframe 'df2'
+for i in range(len(LocationLatitude)): #For every row 'i' in the range 0 to the length of the array 'LocationLatitude'
+    for x in range(len(PortsLatitude)): #For every row 'x' in the range 0 to the length of the array 'PortsLatitude'
+        costs.append((((LocationLatitude[i] - PortsLatitude[x])**2+ \
+        (LocationLongitude[i]-PortsLongitude[x])**2)**(0.5))* \
+        LocationProduction[i]) # Add to the array 'costs' by subtracting square of the x'th element of Ports Latitude from the i'th element of LocationLatitude and adding this to the square of the subtraction 
+        #of the x'th element of PortsLongitude from the i'th element of LocationLongitude and the finding the square root of this sum - Effectively, using Pythagerous Theorem to find the distance between the points
+        locations.append(i+1) # appending the array 'Locations' with the number 'i' and adding 1 to it to transform it to a real world number - this is done at the same point as the cost for the i'th element calculation to simulate a dictionary creation
+        portslocation.append(x+1) # appending the array 'PortsLocations' with the number 'x' and adding 1 to it to transform it to a real world number - this is done at the same point as the cost for the x'th element calculation to simulate a dictionary creation
+
+
+print "The location that gives the lowest transport costs is Location " + str(locations[np.argmin(costs)]) + " and Port " + str( portslocation[np.argmin(costs)]) + " giving a total cost of €"+ str( costs[np.argmin(costs)])
+# print the above string with the text and using the Numpy library function 'argmin' to find which element of the costs array is the lowest - this turns out to be 14, so the index 14 is looked up Loctaions and PortsLocations array
